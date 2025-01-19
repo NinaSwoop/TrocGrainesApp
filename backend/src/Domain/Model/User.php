e2699c5bf7a5a5683186d6cf3efb422dca829d86
Nina IACOPONELLI <nina.iacoponelli@gmail.com>
2025-01-19 18:47:45 +0100

    build: create User model

---
+++ backend/src/Domain/Model/User.php
@@ -0,0 +1,128 @@
+<?php
+
+class User
+{
+    private int $id;
+    private string $username;
+    private string $firstname;
+    private string $lastname;
+    private string $email;
+    private DateTime $birthdate;
+    private string $picture;
+    private string $password;
+    private int $pointBalance;
+    private string $role;
+    private DateTime $createdAt;
+    private DateTime $updatedAt;
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    public function getFirstname(): string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): void
+    {
+        $this->firstname = $firstname;
+    }
+
+    public function getLastname(): string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): void
+    {
+        $this->lastname = $lastname;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getBirthdate(): DateTime
+    {
+        return $this->birthdate;
+    }
+
+    public function setBirthdate(DateTime $birthdate): void
+    {
+        $this->birthdate = $birthdate;
+    }
+
+    public function getPicture(): string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(string $picture): void
+    {
+        $this->picture = $picture;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getPointBalance(): int
+    {
+        return $this->pointBalance;
+    }
+
+    public function setPointBalance(int $pointBalance): void
+    {
+        $this->pointBalance = $pointBalance;
+    }
+
+    public function getRole(): string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): void
+    {
+        $this->role = $role;
+    }
+
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getUpdatedAt(): DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(DateTime $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+}
