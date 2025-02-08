360906af5c8049523e52a27736cd9faf5aa323f3
Nina IACOPONELLI <nina.iacoponelli@gmail.com>
2025-02-08 10:51:44 +0100

    feat/TAC-266: improve display on desktop

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import logo from "../assets/logo.svg";
 import userLogo from "../assets/user.svg";
 import helpdeskLogo from "../assets/helpdesk.svg";
 import leaveLogo from "../assets/leave.svg";
+import placeholder from "../assets/placeholder-user.webp";
 export default function Navbar() {
     const { user, logout } = useContext(AuthContext);
 
@@ -18,45 +19,59 @@ export default function Navbar() {
     };
 
     return (
-        <nav className="w-full fixed top-0 flex flex-col sm:flex-row justify-between items-center bg-beige p-4 z-50">
-            <div className="flex items-center mb-4 sm:mb-0">
-                <a href="/" className="flex items-center">
-                    <img src={logo} alt="logo" className="h-10 mr-2"/>
-                    <p className="text-lg">TROC & GRAINES</p>
-                </a>
-            </div>
-            <div className="flex items-center mb-4 sm:mb-0">
-                <div className="flex items-center">
-                    {user ? (
-                        <>
-                            <button onClick={() => window.location.href = "/help_points"} className="flex items-center text-green-500">
-                                <p className="text-lg mr-2">{user.point_balance}</p>
-                                <img src={leaveLogo} alt="user icon" className="h-10 mr-8"/>
-                            </button>
-                        </>
-                    ) : (
-                       ""
-                    )}
-                </div>
-                <div className="flex items-center mb-4 sm:mb-0">
-                    <button onClick={() => window.location.href = "/helpdesk"} className="flex items-center text-green-500">
-                        <img src={helpdeskLogo} alt="helpdesk icon" className="h-10 mr-8"/>
-                    </button>
+        <nav className="bg-amber-500 p-4 hidden md:block">
+            <div className="flex justify-between">
+                <div className="">
+                    <a href="/" className="flex items-center">
+                        <img src={logo} alt="logo" className="h-10"/>
+                        <p className="text-l">TROC & GRAINES</p>
+                    </a>
                 </div>
                 <div className="flex items-center">
-                    {user ? (
-                        <>
-                            <button onClick={handleLogout} className="flex items-center text-green-500">
-                                <img src={userLogo} alt="user icon" className="h-10 mr-2"/>
-                                Déconnexion
-                            </button>
-                        </>
-                    ) : (
-                        <button onClick={() => window.location.href = "/login"} className="flex items-center text-green-500">
-                            <img src={userLogo} alt="user icon" className="h-10 mr-2"/>
-                            <p className="text-lg">Connexion</p>
+                    <div className="flex items-center">
+                        <div className="flex items-center">
+                            {user ? (
+                                <button onClick={() => window.location.href = "/profile"} className="flex items-center green-darker">
+                                    <p className="text-m mr-2">{user.username}</p>
+                                    <img src={placeholder} alt="avatar of plant" className="h-10 mr-8 cursor-pointer aspect-square rounded-4xl"/>
+                                </button>
+                            ) : (
+                                ""
+                            )}
+                        </div>
+                        <div className="">
+                            {user ? (
+                                    <button onClick={() => window.location.href = "/help_points"} className="flex items-center green-darker">
+                                        <p className="text-l mr-2">{user.point_balance}</p>
+                                        <img src={leaveLogo} alt="leave icon" className="h-7 mr-8 cursor-pointer"/>
+                                    </button>
+                            ) : (
+                               ""
+                            )}
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <button onClick={() => window.location.href = "/helpdesk"} className="flex items-center green-darker cursor-pointer">
+                            <img src={helpdeskLogo} alt="helpdesk icon" className="h-10 mr-2"/>
+                            <p className="text-l mr-8">Guide</p>
                         </button>
-                    )}
+                    </div>
+                    <div className="">
+                        {user ? (
+                            <>
+                                <button onClick={handleLogout} className="flex items-center green-darker cursor-pointer">
+                                    <img src={userLogo} alt="user icon" className="h-10 mr-2"/>
+                                    <p className="text-l">Déconnexion</p>
+                                </button>
+                            </>
+                        ) : (
+                            <button onClick={() => window.location.href = "/login"} className="flex items-center green-darker cursor-pointer">
+                                <img src={userLogo} alt="user icon" className="h-5 mr-2"/>
+                                <p className="text-l">Connexion</p>
+                            </button>
+                        )}
+                    </div>
                 </div>
             </div>
         </nav>
